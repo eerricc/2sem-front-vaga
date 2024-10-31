b8cd29ae60ecc979e724ff9fc3dda0cb228c982d
eeric <eeric.segawa@gmail.com>
2024-10-31 11:11:37 -0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
-
+import { Link } from "react-router-dom"
 
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='bg-[#D9D9D9] flex justify-center'>
+      <div className="flex w-[90%] justify-between p-5">
+        <h1>Titulo</h1>
+        <nav className="w-[20%]">
+          <ul className="flex justify-between">
+            <li><Link>Botao1</Link></li>
+            <li><Link>Botao2</Link></li>
+            <li><Link className="bg-red-300 rounded-full py-2 px-3">Botao3</Link></li>
+          </ul>
+        </nav>
+        </div>
+    </header>
   )
 }
 
