6b084fb8b71417e9dcbe8034e826a5a0ff389621
eeric <eeric.segawa@gmail.com>
2024-10-31 11:44:13 -0300

    arrumando header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='bg-[#D9D9D9] flex justify-center'>
-      <div className="flex w-[90%] justify-between p-5">
-        <h1>Titulo</h1>
+    <header className='bg-[#602540] flex justify-center'>
+      <div className="flex w-[90%] justify-between p-10 items-center">
+        <h1 className="font-bold text-[#FF9FC5] text-5xl">Herois</h1>
         <nav className="w-[20%]">
-          <ul className="flex justify-between">
-            <li><Link>Botao1</Link></li>
-            <li><Link>Botao2</Link></li>
-            <li><Link className="bg-red-300 rounded-full py-2 px-3">Botao3</Link></li>
+          <ul className="flex justify-between text-[#FF9FC5] font-bold">
+            <li><Link>Home</Link></li>
+            <li><Link>Produtos</Link></li>
+            <li><Link className="bg-white rounded-full py-4 px-6">Login</Link></li>
           </ul>
         </nav>
         </div>
